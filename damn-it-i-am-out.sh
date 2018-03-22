42fbfb4040e47b6e162c265a795da3b9b3e93fd0
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2018-03-22 18:26:31 +0100

    New options: create-conf, remove-conf. Coonf file is now stored in HOME dir

--- damn-it-i-am-out.sh
+++ damn-it-i-am-out.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "-h" ]; then
-    echo "help"
+    echo "damn-it-i-am-out v1.0"
+    echo "create-conf - aby stworzyć plik konfiguracyjny"
+    echo "remove-conf - aby skasować plik konfiguracyjny"
     exit 0;
 fi
 
@@ -9,14 +11,42 @@ command -v convert > /dev/null 2>&1 || { echo >&2 "Imagemagick nie jest zainstal
 #command -v gpg     > /dev/null 2>&1 || echo >&2 "gpg required.  Aborting."; exit 1;  
 
 configFile="$HOME/.damn-it-i-am-out.conf"
+configFileEncrypted="$HOME/.damn-it-i-am-out.conf.gpg"
+
+if [ "$1" == "remove-conf" ]; then
+    rm -f $configFileEncrypted
+    exit 0;
+fi
 
 if [ "$1" == "create-conf" ]; then
     command -v gpg  > /dev/null 2>&1 || { echo >&2 "GNU Privacy Guard nie jest zainstalowany. Kończę działanie programu. "; exit 1; } 
     echo "create-conf"
     
     echo "#stworzono za pomocą skryptu" > $configFile
-    read -p '[Imię:] ' var
+    read -p 'Miasto: ' var
+    echo : $var >> $configFile
+    read -p 'Imię: ' var
     echo Imię: $var >> $configFile
+    read -p 'Imię narzędnik: ' var
+    echo Imię narzędnik: $var >> $configFile
+    read -p 'Nazwisko: ' var
+    echo Nazwisko: $var >> $configFile
+    read -p 'Nazwisko narzędnik: ' var
+    echo Nazwisko narzędnik: $var >> $configFile
+    read -p 'Adres: ' var
+    echo Adres: $var >> $configFile
+    read -p 'Adres c.d.: ' var
+    echo Adres c.d.: $var >> $configFile
+    read -p 'Nazwa pracodawcy: ' var
+    echo Nazwa pracodawcy: $var >> $configFile
+    read -p 'Adres pracodawcy: ' var
+    echo Adres pracodawcy: $var >> $configFile
+    read -p 'Adres pracodawcy c.d.: ' var
+    echo Adres pracodawcy c.d.: $var >> $configFile
+    read -p 'Data podpisania umowy dd.mm.rrrr ' var
+    echo Data podpisania umowy: $var >> $configFile
+    read -p 'Miejsce podpisania umowy: ' var
+    echo Miejsce podpisania umowy: $var >> $configFile
     
     gpg -c --cipher-algo AES256 $configFile
     rm -f $configFile
@@ -27,19 +57,45 @@ blankPage="blankPage.pdf"
 outputPage="wypowiedzenie.pdf"
 currentDate=`date +%d.%m.%Y`
 
-configFilePath="./.damn-it-i-am-out.conf"
-city=`cat $configFilePath | sed -n 's/^Miasto: \(.*\)$/\1/p'`
-name=`cat $configFilePath | sed -n 's/^Imię: \(.*\)$/\1/p'`
-nameInstrumentalis=`cat $configFilePath | sed -n 's/^Imię narzędnik: \(.*\)$/\1/p'`
-surname=`cat $configFilePath | sed -n 's/^Nazwisko: \(.*\)$/\1/p'`
-surnameInstrumentalis=`cat $configFilePath | sed -n 's/^Nazwisko narzędnik: \(.*\)$/\1/p'`
-address=`cat $configFilePath | sed -n 's/^Adres: \(.*\)$/\1/p'`
-address2nd=`cat $configFilePath | sed -n 's/^Adres c.d.: \(.*\)$/\1/p'`
-employerName=`cat $configFilePath | sed -n 's/^Nazwa pracodawcy: \(.*\)$/\1/p'`
-employerAddress=`cat $configFilePath | sed -n 's/^Adres pracodawcy: \(.*\)$/\1/p'`
-employerAddress2nd=`cat $configFilePath | sed -n 's/^Adres pracodawcy c.d.: \(.*\)$/\1/p'`
-contractDate=`cat $configFilePath | sed -n 's/^Data podpisania umowy: \(.*\)$/\1/p'`
-contractSigningCity=`cat $configFilePath | sed -n 's/^Miejsce podpisania umowy: \(.*\)$/\1/p'`
+if [ -f $configFileEncrypted ]; then
+    echo "Znaleziono zaszyfrowany plik configuracyjny"
+    echo "odszyfrowanie....:"
+    gpg -d --cipher-algo AES256 $configFileEncrypted > $configFile
+    echo "Odszyfrowano plik."
+
+    # configFilePath="./.damn-it-i-am-out.conf"
+    configFilePath=$configFile
+    city=`cat $configFilePath | sed -n 's/^Miasto: \(.*\)$/\1/p'`
+    name=`cat $configFilePath | sed -n 's/^Imię: \(.*\)$/\1/p'`
+    nameInstrumentalis=`cat $configFilePath | sed -n 's/^Imię narzędnik: \(.*\)$/\1/p'`
+    surname=`cat $configFilePath | sed -n 's/^Nazwisko: \(.*\)$/\1/p'`
+    surnameInstrumentalis=`cat $configFilePath | sed -n 's/^Nazwisko narzędnik: \(.*\)$/\1/p'`
+    address=`cat $configFilePath | sed -n 's/^Adres: \(.*\)$/\1/p'`
+    address2nd=`cat $configFilePath | sed -n 's/^Adres c.d.: \(.*\)$/\1/p'`
+    employerName=`cat $configFilePath | sed -n 's/^Nazwa pracodawcy: \(.*\)$/\1/p'`
+    employerAddress=`cat $configFilePath | sed -n 's/^Adres pracodawcy: \(.*\)$/\1/p'`
+    employerAddress2nd=`cat $configFilePath | sed -n 's/^Adres pracodawcy c.d.: \(.*\)$/\1/p'`
+    contractDate=`cat $configFilePath | sed -n 's/^Data podpisania umowy: \(.*\)$/\1/p'`
+    contractSigningCity=`cat $configFilePath | sed -n 's/^Miejsce podpisania umowy: \(.*\)$/\1/p'`
+    firstDay=`echo $contractDate |sed -n 's/^\([[:digit:]]*\).\([[:digit:]]*\).\([[:digit:]]*\)/\2\/\1\/\3/p'`
+else
+    echo "Nie znaleziono pliku konfiguracyjnego"
+    echo "Proszę podać dane:"
+    
+    read -p '[Miasto:] ' city
+    read -p '[Imię:] ' name
+    read -p '[Imię narzędik:] ' nameInstrumentalis
+    read -p '[Nazwisko:] ' surname
+    read -p '[Nazwisko narzędnik:] ' surnameInstrumentalis
+    read -p '[Adres:] ' address
+    read -p '[Adres c.d.:] ' address2nd
+    read -p '[Nazwa pracodawcy:] ' employerName
+    read -p '[Adres pracodawcy:] ' employerAddress
+    read -p '[Adres pracodawcy c.d.:] ' employerAddress2nd
+    read -p '[Data podpisania umowy dd.mm.rrrr:] ' contractDate
+    read -p '[Miejsce podpisania umowy:] ' contractSigningCity
+
+fi
 
 firstDay=`echo $contractDate |sed -n 's/^\([[:digit:]]*\).\([[:digit:]]*\).\([[:digit:]]*\)/\2\/\1\/\3/p'`
 contractDateF=`date -d $firstDay +%Y%m%d`
@@ -81,4 +137,5 @@ convert $blankPage -font Helvetica \
 
 rm -f $blankPage
 
+echo "Pomyślnie wygenerowano plik $outputPage"
 
